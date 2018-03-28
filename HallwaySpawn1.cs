2c32f9f7ef4ccf52c65a7e6e304f8c2346223487
Erik Wiker <erikdw3@vt.edu>
2018-03-28 12:06:06 -0400

    Hallway spawner for deactivating/activating prefabs, trigger button script
    These two files are proof of concept for using triggers to deactivate/activate prefabs.
    
    Also, Red script is used to test a button press to trigger something.

---
+++ HallwaySpawn1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HallwaySpawn1 : MonoBehaviour {
+
+    public GameObject DespawnedPrefab;
+    public GameObject SpawnedPrefab;
+    
+    public GameObject SpawnLocation;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag ("Player"))
+        {
+            //Only happens once
+            if (this.enabled)
+            {
+                this.enabled = false;
+                SpawnedPrefab.SetActive(true);
+                DespawnedPrefab.SetActive(false);
+
+                //Instantiate(SpawnedPrefab, SpawnLocation.transform.position, SpawnLocation.transform.rotation);
+                //Destroy(DespawnedPrefab);
+                Debug.Log("Triggered HallwaySpawner1");
+            }
+        }
+    }
+}
---
+++ RedScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedScript : MonoBehaviour {
+
+    Animator anim;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetTrigger("Trigger");
+            Debug.Log("Triggered Animation");
+        }
+	}
+}
