7cb948b04e778419f42c0c669745fd69ca2224b3
Shuangfei Fan <sophia23@vt.edu>
2018-04-01 13:08:41 -0400

    Add scripts for collecting keys and open doors

---
+++ Door.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour 
+{
+    bool locked= true;
+    bool doorOpen;
+	private Animator animator;
+
+	private AudioSource audioSource;
+
+	void Start() {
+		doorOpen = false;
+		audioSource = GetComponent<AudioSource>();
+		animator = GetComponent<Animator> ();
+
+	}
+
+	void onTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
+			Debug.Log("Trigger collided with the player:door");
+			doorOpen = true;
+			Doors ("Open");
+
+		}
+
+	}
+
+	void Doors(string direction) {
+		animator.SetTrigger(direction);
+	}
+
+
+
+
+
+}
---
+++ Key.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Key : MonoBehaviour 
+{
+    //public GameObject KeyPoofPrefab;//Create a reference to the KeyPoofPrefab and Door
+    //public Door DoorRef;
+
+	private AudioSource audioSource;
+
+
+	void Start(){
+
+		audioSource = GetComponent<AudioSource>();
+	}
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+		
+	public void OnTriggerEnter(Collider other)
+	{
+
+		if (other.gameObject.tag == "Player") {
+			AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
+
+			Debug.Log("Trigger collided with the player:key");
+
+			Destroy (this.gameObject);
+		}
+	}
+
+
+
+}
